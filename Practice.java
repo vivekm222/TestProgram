86d7d05c12c018e52dca29916b60a2b7085b1f8a
vivekm222 <73857950+vivekm222@users.noreply.github.com>
2022-01-24 21:11:09 -0700

    testing

---
+++ Practice.java
@@ -0,0 +1,16 @@
+class test{
+
+    public static int sum(int a, int b){
+        return a+b;
+
+    }
+
+
+public static void main(String[]args){
+
+    System.out.println("Hello world!!!");
+    System.out.println(sum(1,2));
+
+}
+
+}
